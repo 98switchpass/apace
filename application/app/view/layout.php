dfbad09b8312fe350ce1d49a39df92b4dc1f4d02
apacedev <apacedev@gmail.com>
2017-03-04 20:17:29 +0100

    Updated layout.php

--- application/app/view/layout.php
+++ application/app/view/layout.php
@@ -4,17 +4,12 @@
 
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-
 	<title>Apace</title>
-
-	<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
-    <meta name="viewport" content="width=device-width" />
+	<meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0'  />
 
 	<link href="<?=Apace::getDataUrl('css/minified');?>layout.css" rel="stylesheet"/>
 
 </head>
-
-
 
 <body>
 
